acd5c187978013023937978a5057f312c0c6ad5f
Can Ahmet ACAR <64232218+canacar7@users.noreply.github.com>
2021-04-18 23:52:21 +0300

    Update hafizadaki_sayiyi_1_byte_alma_kaydetme.s

--- stack_subroutines/hafizadaki_sayiyi_1_byte_alma_kaydetme.s
+++ stack_subroutines/hafizadaki_sayiyi_1_byte_alma_kaydetme.s
@@ -7,12 +7,12 @@ __main
 	
 			LDR 	R0, =0X20000040
 			LDRB 	R1,[R0]				;hafizada kayitli sayinin en anlamsiz 1 byte ni R1 e atadik
-			BFC		R1, #4, #4 		    ;R1 de bulunan degerinin en anlamsiz 4 bitine ulastik BFC 4. bitten 8. bite kadar anlamli bitleri sildi
+			BFC	R1, #4, #4 		        ;R1 de bulunan degerinin en anlamsiz 4 bitine ulastik BFC 4. bitten 8. bite kadar anlamli bitleri sildi
 			STRB	R1,[R0,#4]			;memory de kaydecegimiz yeri tanimladik R0 + 4
 			
 			LDRB	R1,[R0]				;hafizada kayitli sayinin en anlamsiz 1 byte ni R1 e atadik
-			LSR		R1,R1,#4			;degeri 4 bit saga kaydirdik ve en anlamli 4 bitine ulastik
+			LSR	R1,R1,#4			;degeri 4 bit saga kaydirdik ve en anlamli 4 bitine ulastik
 			STRB	R1,[R0,#5]			;memory de kaydecegimiz yeri tanimladik R0 + 5
 		
-loop 		B		loop
+loop 			B		loop
 			END
